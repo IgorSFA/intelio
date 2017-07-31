e14f2779cc3b3bb50876c35e6ebdd121b477724f
Igor Souza <igorsfdea@gmail.com>
2017-07-31 22:08:14 +0000

    Last commit

--- receberCompra.php
+++ receberCompra.php
@@ -1,6 +1,6 @@
 <?php
 	if(isset($_POST['message'])){
-		$myfile = fopen("compras.csv", "a") or die("Unable to open file!");
+		$myfile = fopen(dirname(__FILE__) . "/apiori/compras.csv", "a") or die("Unable to open file!");
 		fwrite($myfile, $_POST['message'] . PHP_EOL);
 		fclose($myfile);
 	}
